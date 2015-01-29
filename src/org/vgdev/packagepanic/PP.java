102bb1ef540920814e66ee8e5e6b9a0ae9a38faf
Jay Rama Fisher <jay.rama.fisher@gmail.com>
2015-01-28 23:45:56 -0500

    This commit fixes the grid dimensions and adds the jar for easy downloading.

--- src/org/vgdev/packagepanic/PP.java
+++ src/org/vgdev/packagepanic/PP.java
@@ -5,8 +5,8 @@ public class PP {
   public static final boolean DEBUG = true;
 
   //the number of grid cells in each direction
-  public static final int NX = 11;
-  public static final int NY = 15;
+  public static final int NX = 10;
+  public static final int NY = 14;
 
   //the size of each grid cell in pixels
   public static final int SZ = 50;
